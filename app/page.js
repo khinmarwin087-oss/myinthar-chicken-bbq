183593ddc8384fe50fbeaaa2d8fc8daa5ae84113
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-11 21:14:52 +0630

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>My VPN-Free Site</h1>
+      <p>ဒီ Website က Firebase ကို VPN မလိုဘဲ သုံးမှာပါ။</p>
+      {/* မင်းရဲ့ HTML တွေကို ဒီကြားထဲမှာ ထည့်လို့ရတယ် */}
+    </div>
+  );
+}
+
