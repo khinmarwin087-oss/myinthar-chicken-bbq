5be937d47f2feebe1fbc2f268c439560c54ff53b
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-12 14:57:02 +0630

    Update page.js

--- app/customer_menu/page.js
+++ app/customer_menu/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-
+import Image from 'next/image';
 import { db } from "../../lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
