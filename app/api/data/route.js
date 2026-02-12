c7ff73cfc708ad1eb941316d7014e737192b991c
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-12 13:12:18 +0630

    Update route.js

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,4 +1,4 @@
-import { db } from "@/lib/firebase";
+import { db } from "../../../lib/firebase"; 
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
