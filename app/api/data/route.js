d356eb3f16fdb1f2538a9aa689c3e166cf7d17ca
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-15 12:35:21 +0630

    Update route.js

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,4 +1,3 @@
-export const dynamic = 'force-dynamic'; // ဒါလေး ထည့်ပေးပါ
 
 import { db } from "../../../lib/firebase"; 
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from "firebase/firestore";
